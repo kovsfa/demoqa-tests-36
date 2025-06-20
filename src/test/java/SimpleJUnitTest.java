dd3a7fb9af96c8c0ebfb1886bae300092bd584a6
Sofia Kovaleva <kov.sfa@gmail.com>
2025-06-20 17:09:21 +0500

    added JUnit 5 and Simple Test

---
+++ src/test/java/SimpleJUnitTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.*;
+
+public class SimpleJUnitTest {
+
+    int result;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("\n### beforeAll()\n");
+
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("###     beforeEach()");
+        result = getResult();
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("###     afterEach()\n");
+        result = 0;
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("### afterAll()\n");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("###     firstTest()");
+        Assertions.assertTrue(3 > 2);
+    }
+    @Test
+    void secondTest() {
+        System.out.println("###     secondTest()");
+        Assertions.assertTrue(3 > 2);
+    }
+    @Test
+    void thirdTest() {
+        System.out.println("###     thirdTest()");
+        Assertions.assertTrue(3 > 2);
+    }
+    private int getResult() {
+        return 3;
+    }
+}
